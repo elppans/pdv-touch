b96f603d0e17b1cb715af864bf2dfb422d9f1d22
elppans <elppansmk@gmail.com>
2024-10-03 11:08:47 -0300

    Alterado funções

--- PDVTouch.sh
+++ PDVTouch.sh
@@ -11,58 +11,60 @@ done
 }
 
 # Função para verificar e posicionar a janela Java
-pdvjava_param() {
-  while true; do
-    WMID=$(wmctrl -l | grep "Zanthus Retail" | cut -d " " -f1)
-    if [ -z "$WMID" ]; then
-      echo "Aguardando 'Zanthus Retail' iniciar..."
-      sleeping 5
-      clear
-    else
-      # Garantir que o Java seja configurado na posição parametrizada.
-      wmctrl -i -r $WMID -e "0,$posicaox1,-1,-1"
-      echo "Janela 'Zanthus Retail' encontrada e configurada."
-      break
-    fi
-  done
-}
+# pdvjava_param() {
+#   while true; do
+#     WMID=$(wmctrl -l | grep "Zanthus Retail" | cut -d " " -f1)
+#     if [ -z "$WMID" ]; then
+#       echo "Aguardando 'Zanthus Retail' iniciar..."
+#       sleeping 5
+#       clear
+#     else
+#       # Garantir que o Java seja configurado na posição parametrizada.
+#       wmctrl -i -r $WMID -e "0,$posicaox1,-1,-1"
+#       echo "Janela 'Zanthus Retail' encontrada e configurada."
+#       break
+#     fi
+#   done
+# }
 
 # Função para executar o Java (base PDVJava)
 pdvjava_exec() {
-/usr/bin/unclutter 1> /dev/null &
+# /usr/bin/unclutter 1> /dev/null &
 chmod +x /usr/local/bin/igraficaJava
 chmod -x /usr/local/bin/dualmonitor_control-PDVJava
 nohup dualmonitor_control-PDVJava &>>/dev/null &
-nohup igraficaJava &>>/dev/null &
+# nohup igraficaJava &>>/dev/null &
 nohup recreate-user-rabbitmq.sh &>>/dev/null &
 echo "Iniciando pdvJava2..."
 nohup xterm -e "/Zanthus/Zeus/pdvJava/pdvJava2" &>>/dev/null &
-pdvjava_param
+# pdvjava_param
+sleeping 25
 }
 
-# Função para executar o Painel Chama Fila
-painel_exec() {
+# Função para executar o Interface
+interface_exec() {
 # Configuração de Profile e Storage
 local temp_profile
 local local_storage
 
-temp_profile="$HOME/.painel/chromium"
+temp_profile="$HOME/.interface/chromium"
 local_storage="$temp_profile/Default/Local Storage"
 
 mkdir -p "$local_storage"
 
-echo "Iniciando Painel..."
+echo "Iniciando interface..."
 sleeping 10
 
-# Limpar informações de profile, mas manter configuração do Painel
+# Limpar informações de profile, mas manter configuração do interface
 find "$temp_profile" -mindepth 1 -not -path "$local_storage/*" -delete &>>/dev/null
 
 # Executar Chromium com uma nova instância
 setsid nohup chromium-browser --no-sandbox \
 --test-type \
 --no-default-browser-check \
+--no-context-menu \
+--disable-gpu \
 --disable-session-crashed-bubble \
---restore-last-session=false \
 --disable-infobars \
 --disable-background-networking \
 --disable-component-extensions-with-background-pages \
@@ -70,16 +72,19 @@ setsid nohup chromium-browser --no-sandbox \
 --disable-restore-session-state \
 --disable-features=DesktopPWAsAdditionalWindowingControls \
 --disable-features=TabRestore \
+--disable-translate \
+--disk-cache-dir=/tmp/chromium-cache \
 --user-data-dir="$temp_profile" \
+--restore-last-session=false \
 --autoplay-policy=no-user-gesture-required \
 --enable-speech-synthesis \
 --kiosk \
-http://127.0.0.1:9090/moduloPHPPDV/painel.php --window-position="$posicaox2" &>>/dev/null &
+file:////Zanthus/Zeus/Interface/index.html &>>/dev/null &
 }
 
 # Execução das funções
 pdvjava_exec
-painel_exec
+interface_exec
 
 # Finalização
 echo "Esta janela será fechada após..."
