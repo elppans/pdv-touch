e9629ff31f6e3367e9859ba99204c5e716e519db
elppans <elppansmk@gmail.com>
2025-07-31 15:06:52 -0300

    Create PDVTouch_Simples_Perto.sh

---
+++ PDVTouch_Simples_Perto.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pdv_data="$HOME/.interface"
+export pdv_data
+sudo mkdir -p "$pdv_data"
+sudo chmod -R 777 "$pdv_data"
+
+chmod -x /usr/local/bin/igraficaJava
+chmod -x /usr/local/bin/dualmonitor_control-PDVJava
+nohup recreate-user-rabbitmq.sh &
+/Zanthus/Zeus/pdvJava/pdvJava2 &
+/Zanthus/Zeus/pdvJava/x11vnc.sh &
+/root/self_perto_config.sh &
+
+sleep 20
+
+nohup chromium-browser --disable-gpu \
+--user-data-dir="$pdv_data" \
+--no-sandbox \
+--kiosk \
+--no-context-menu \
+--disable-translate file:////Zanthus/Zeus/Interface/index.html
+#--disk-cache-dir=/tmp/chromium-cache
