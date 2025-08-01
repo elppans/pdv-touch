e9498e9311b3aadf6a468a33627d7e3c0b71b725
elppans <elppansmk@gmail.com>
2025-08-01 15:13:51 -0300

    Update PDVTouch_Simples_Perto.sh

--- PDVTouch_Simples_Perto.sh
+++ PDVTouch_Simples_Perto.sh
@@ -12,7 +12,7 @@ nohup recreate-user-rabbitmq.sh &
 /Zanthus/Zeus/pdvJava/pdvJava2 &
 /Zanthus/Zeus/pdvJava/x11vnc.sh &
 /root/self_perto_config.sh &
-/Zanthus/Zeus/monitor-pos-set.sh | tee /tmp/monitor-pos-set.log
+/Zanthus/Zeus/monitor-pos-set-perto.sh | tee /tmp/monitor-pos-set-perto.log
 
 sleep 20
 
