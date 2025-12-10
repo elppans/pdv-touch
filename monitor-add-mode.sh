ea408ff7ad83586a86e85f9ff665f149e34d6c72
elppans <elppansmk@gmail.com>
2025-12-10 09:20:29 -0300

    Update monitor-add-mode.sh

--- monitor-add-mode.sh
+++ monitor-add-mode.sh
@@ -11,14 +11,21 @@
 
 # Monitor 1
 
-MONITOR1='HDMI-1'
-LARG='1920'
-ALT='1080'
-FREQ='60'
+MONITOR1='HDMI-1'  # Nome da saída de vídeo (monitor conectado via HDMI)
+LARG='1920'        # Largura da resolução desejada (pixels)
+ALT='1080'         # Altura da resolução desejada (pixels)
+FREQ='60'          # Frequência de atualização desejada (Hz)
 
+# Gera um "Modeline" com base na resolução e frequência informadas e salva em /tmp/cvt_mode
 cvt "$LARG $ALT $FREQ" > /tmp/cvt_mode
+
+# Cria um novo modo de vídeo no xrandr usando o Modeline gerado
 xrandr --newmode $(grep 'Modeline' /tmp/cvt_mode | sed 's/Modeline//')
+
+# Associa o novo modo ao monitor especificado
 xrandr --addmode "$MONITOR1" $(grep 'Modeline' /tmp/cvt_mode | sed 's/Modeline//' | awk '{print $1}')
+
+# Define o monitor para usar o novo modo criado
 xrandr --output "$MONITOR1" --mode $(grep 'Modeline' /tmp/cvt_mode | sed 's/Modeline//' | awk '{print $1}')
 
 # Monitor 2 (Descomente as linhas se quiser usar)
